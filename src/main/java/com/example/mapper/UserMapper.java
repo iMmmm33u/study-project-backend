1413d381c0c1f5a29cb8b6521e710ed9a023b3bd
wcx <wcx202407@163.com>
2025-09-12 22:34:49 +0800

    完成邮件验证码发送

---
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.mapper;
+
+import com.example.entity.Account;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("select * from db_account where username = #{text} or email = #{text}")
+    Account findAccountByNameOrEmail(@Param("text") String text);
+}
---
+++ src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
@@ -0,0 +1,77 @@
+package com.example.service.impl;
+
+import com.example.entity.Account;
+import com.example.mapper.UserMapper;
+import com.example.service.AuthorizeService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class AuthorizeServiceImpl implements AuthorizeService {
+
+    @Value("${spring.mail.username}")
+    String from;
+
+    @Resource
+    UserMapper mapper;
+
+    @Resource
+    MailSender mailSender;
+
+    @Resource
+    StringRedisTemplate template;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username == null) {
+            throw new UsernameNotFoundException("用户名不能为空");
+        }
+        Account account = mapper.findAccountByNameOrEmail(username);
+        if (account == null) {
+            throw new UsernameNotFoundException("用户名或密码错误");
+        }
+        return User
+                .withUsername(account.getUsername())
+                .password(account.getPassword())
+                .roles("user")
+                .build();
+    }
+
+    @Override
+    public boolean sendValidateEmail(String email, String sessionId) {
+        String key = "email:" + sessionId + ":" + email;
+        if (template.hasKey(key)) {
+            Long expire = Optional.ofNullable(template.getExpire(key, TimeUnit.SECONDS)).orElse(180L);
+            if (expire > 120) {
+                return false;
+            }
+        }
+        Random random = new Random();
+        int code = random.nextInt(899999) + 100000;
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(email);
+        message.setSubject("您的验证邮件");
+        message.setText("验证码是: " + code);
+        try {
+            mailSender.send(message);
+            template.opsForValue().set(key, String.valueOf(code), 3, TimeUnit.MINUTES);
+            return true;
+        } catch (MailException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
