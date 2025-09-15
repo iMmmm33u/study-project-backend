990b827c1388468d108db199d687f1b49dda8d7d
wcx <wcx202407@163.com>
2025-09-15 21:41:56 +0800

    项目完成

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.mapper;
 
-import com.example.entity.Account;
+import com.example.entity.auth.Account;
+import com.example.entity.user.AccountUser;
 import org.apache.ibatis.annotations.*;
 
 @Mapper
@@ -8,6 +9,9 @@ public interface UserMapper {
 
     @Select("select * from db_account where username = #{text} or email = #{text}")
     Account findAccountByNameOrEmail(@Param("text") String text);
+
+    @Select("select * from db_account where username = #{text} or email = #{text}")
+    AccountUser findAccountUserByNameOrEmail(@Param("text") String text);
 
     @Insert("insert into db_account (username, password, email) values (#{username}, #{password}, #{email})")
     int createAccount(String username, String password, String email);
--- src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
+++ src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
@@ -1,10 +1,9 @@
 package com.example.service.impl;
 
-import com.example.entity.Account;
+import com.example.entity.auth.Account;
 import com.example.mapper.UserMapper;
 import com.example.service.AuthorizeService;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.mail.MailException;
